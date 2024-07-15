c1d95e8644b6e7e694a4e8c92cca837f3a5a7892
sohamdasx <sohamdas.2702@gmail.com>
2024-07-15 22:16:57 +0530

    initialized and set up

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on  port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
